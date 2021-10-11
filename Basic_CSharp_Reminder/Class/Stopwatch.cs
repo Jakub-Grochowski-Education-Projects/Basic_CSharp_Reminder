2820883dd9944b9c7b024f34b20e8cccb9edd948
Jakub Grochowski <grochowski.jakub96@gmail.com>
2021-10-11 12:25:31 +0200

    Add project files.

---
+++ Basic_CSharp_Reminder/Class/Stopwatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basic_CSharp_Reminder.Class
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTIme;
+        public Boolean _isStarted { get; set; }
+
+        public DateTime StartTime
+        {
+            get { return _startTime; }
+            set { _startTime = DateTime.Now; }
+        }
+    }
+}
---
+++ Basic_CSharp_Reminder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_CSharp_Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
